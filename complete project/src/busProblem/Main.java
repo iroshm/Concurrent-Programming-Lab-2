d22e0c93dbe0ec759788b68caa0dd97e0afebbe3
iroshm <36252656+iroshm@users.noreply.github.com>
2020-09-20 19:26:14 +0530

    main updated

--- complete project/src/busProblem/Main.java
+++ complete project/src/busProblem/Main.java
@@ -3,7 +3,7 @@ package busProblem;
 import java.util.Scanner;
 
 
-public class MainClass {
+public class Main {
 	
 	 public static void main(String[] args) throws InterruptedException {
 		 
@@ -11,13 +11,10 @@ public class MainClass {
 	        float riderArrivalMeanTime = 30f * 1000;
 	        float busArrivalMeanTime = 20 * 60f * 1000 ;
 	        
-	        // Get user input
-	        Scanner scanner = new Scanner(System.in);
-	        String userInput;
 	        // Create waiting area object
 	        BusHalt waitingArea = new BusHalt();
 
-	        System.out.println("### PRESS ANY KEY TO STOP ###\n" );
+	        
 	        // Create rider creator object
 	        RiderCreator riderGenerator = new RiderCreator(riderArrivalMeanTime, waitingArea);
 	        (new Thread(riderGenerator)).start();
@@ -25,12 +22,7 @@ public class MainClass {
 	        BusCreator busGenerator = new BusCreator(busArrivalMeanTime,waitingArea);
 	        (new Thread(busGenerator)).start();
 
-	        // Program Termination with a user input
-	        while(true){
-	            userInput = scanner.nextLine();
-	            if(userInput != null)
-	                System.exit(0);
-	        }
+	       
 	    }
 
 
