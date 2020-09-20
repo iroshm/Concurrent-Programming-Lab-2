28486917926ef99b223ebdc3da8418c4b8499705
iroshm <36252656+iroshm@users.noreply.github.com>
2020-09-20 10:43:18 +0530

    main method updated

--- SenateBusProblem.java
+++ SenateBusProblem.java
@@ -138,42 +138,61 @@ public class SenateBusProblem {
     	
     	SenateBusProblem lab = new SenateBusProblem();  
     	
-    	int bus_id=0, rider_id=0;                           //Ids are assigned for buses and riders for the easy demonstration of the program
-        long diff_bus=0,diff_rider=0,time_curr=0,time_prev_bus=System.currentTimeMillis(),time_prev_rider=System.currentTimeMillis();
-        double mean_rider=30000,mean_bus=1200000;           //Declaring the mean of the exponential distributions of inter-arrival times of riders and buses
-        double rand_bus  = 0.0,rand_rider=0,wait_time_rider=0,wait_time_bus=0;
+    	int bid=0;
+    	int rid=0;
+    	
+        long time_difference_of_bus=0;
+        long time_difference_of_rider=0;
+        long current_time=0;
+        long previous_bus=System.currentTimeMillis();
+        long previous_rider=System.currentTimeMillis();
         
-        rand_bus = new Random().nextDouble();   
-        wait_time_rider = Math.round(Math.log10(rand_bus)*-1*mean_rider);  //Calculating the time before the next bus arrives
+        //mean of the exponential distributions of inter-arrival times of riders and buses
+        double riderMean=30000;
+        double busMean=1200000;          
+        double rbus  = 0.0;
+        double rrider=0;
+        double rider_wait=0;
+        double bus_wait=0;
         
-        rand_rider = new Random().nextDouble();   
-        wait_time_bus = Math.round(Math.log10(rand_rider)*-1*mean_bus);     //Calculating the time before the next bus arrives
-        //System.out.println("rand - "+Math.round(rand*100.0)/100.0+ " wait_time_rider -  "+wait_time_rider+" wait_time_bus - "+wait_time_bus);
+        rbus = new Random().nextDouble(); 
+        
+        //To calculate rider waiting time
+        rider_wait = Math.round(Math.log10(rbus)*-1*riderMean);
+        
+        rrider = new Random().nextDouble();   
+        
+      //To calculate bus waiting time
+        bus_wait = Math.round(Math.log10(rrider)*-1*busMean); 
+        
         
         while(Boolean.TRUE) {     
            
-           time_curr=System.currentTimeMillis();
-           diff_rider=time_curr-time_prev_rider;                      //Calculating the time passed after the previous rider
-           diff_bus=time_curr-time_prev_bus;                          //Calculating the time passed after the previous bus
+        	current_time=System.currentTimeMillis();
+        	////time gap between current and previous rider
+        	time_difference_of_rider=current_time-previous_rider;                      
+           //time gap between current and previous bus
+           time_difference_of_bus=current_time-previous_bus;                         
            
-           
-           if(diff_rider==wait_time_rider){                           //Checking if it is the time for the next rider to come
-               //System.out.println("rand - "+Math.round(rand*100.0)/100.0+ " wait_time_rider -  "+wait_time_rider);
-               Rider new_rider = lab.new Rider(rider_id++);
-               new Thread(new_rider).start();
-               time_prev_rider=time_curr;
-                  
-               rand_rider  = new Random().nextDouble();   
-               wait_time_rider = Math.round(Math.log10(rand_rider)*-1*mean_rider); //Calculating the inter arrival time before the next rider arrives
-           }
-           if(diff_bus==wait_time_bus){                           //Checking if it is the time for the next bus to come
-               //System.out.println("rand - "+Math.round(rand*100.0)/100.0+ " wait_time_bus - "+wait_time_bus);
-               Bus new_bus = lab.new Bus(bus_id++);
-               new Thread(new_bus).start();
-               time_prev_bus=time_curr;
+         //if both times are eqaul let new rider to in
+           if(time_difference_of_rider==rider_wait){                           
               
-               rand_bus  = new Random().nextDouble();
-               wait_time_bus = Math.round(Math.log10(rand_bus)*-1*mean_bus);//Calculating the inter arrival time before the next bus arrives
+               Rider new_rider = lab.new Rider(rid++);
+               new Thread(new_rider).start();
+               previous_rider=current_time;
+                  
+               rrider  = new Random().nextDouble();   
+               rider_wait = Math.round(Math.log10(rrider)*-1*riderMean); 
+           }
+           
+         //if both times are eqaul let new bus to in
+           if(time_difference_of_bus==bus_wait){                           
+               Bus new_bus = lab.new Bus(bid++);
+               new Thread(new_bus).start();
+               previous_bus=current_time;
+              
+               rbus  = new Random().nextDouble();
+               bus_wait = Math.round(Math.log10(rbus)*-1*busMean);
            }
        }
        
